d78a84a5999199c48373cfb893a1e9ba2c4478b4
Manex Agirrezabal <manex.agirrezabal@gmail.com>
2020-12-11 10:41:29 +0100

    Fixed error in the program.

--- calculatesum.py
+++ calculatesum.py
@@ -1,8 +1,8 @@
 
 import sys
 
-number1 = float(sys.argv[1])
-number2 = float(sys.argv[2])
+number1 = int(sys.argv[1])
+number2 = int(sys.argv[2])
 
 print ("The sum of the two numbers that you gave me is: ",end="")
-print (number1 * number2)
+print (number1 + number2)
